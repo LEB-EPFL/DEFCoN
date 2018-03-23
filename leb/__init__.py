7c24390100e649ab2306f64ae3c4cb3518d1dc5e
kmdouglass <kyle.m.douglass@gmail.com>
2018-03-23 17:10:55 +0100

    Refactored project structure.

---
+++ leb/__init__.py
@@ -0,0 +1,3 @@
+# © All rights reserved. ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE,
+# Switzerland, Laboratory of Experimental Biophysics
+# See the LICENSE.txt file for more details.
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='DEFCoN',
+    version='0.0.0',
+    packages=['leb', 'leb.defcon'],
+    url='https://github.com/LEB-EPFL/DEFCoN',
+    license='BSD3',
+    author='Baptiste Ottino',
+    author_email='',
+    description='Fluorescence spot density estimation for single molecule microscopy.',
+    install_requires=['numpy', 'scikit-image', 'h5py', 'tensorflow', 'keras', 'pandas']
+)
