ff62ba8a41721e5594fd28591639860a2d7c9d3f
Kyle M. Douglass <kyle.m.douglass@gmail.com>
2018-03-28 17:02:03 +0200

    Add some unit tests for the datasets module.

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -2,13 +2,45 @@
 # Switzerland, Laboratory of Experimental Biophysics, 2018
 # See the LICENSE.txt file for more details.
 
+import tempfile
 import unittest
+
+import h5py
+import tifffile
+
+import leb.defcon.datasets as datasets
 
 
 class TestTrainingSetMethods(unittest.TestCase):
 
-    def test_default(self):
-        assert True
+    def setUp(self):
+        self.tmp_training_set = tempfile.mkstemp()[1]
+
+    def test_init(self):
+        """Tests the creation of a new TrainingSet."""
+        with datasets.TrainingSet(self.tmp_training_set, mode='w') as test_set:
+            pass
+
+
+class TestTrainingSetMethods_Files(unittest.TestCase):
+    """Test case for tests requiring file input/output.
+
+    """
+
+    def setUp(self):
+        self.tmp_training_set = tempfile.mkstemp()[1]
+
+        self.tmp_tiff_stack = tempfile.mkstemp(suffix='.tif')[1]
+        tifffile.imsave(self.tmp_tiff_stack, shape=(2, 2, 1), dtype='uint8')
+
+    def test_add_input_tiff(self):
+        """Tests the addition of a tif image to the dataset."""
+        with datasets.TrainingSet(self.tmp_training_set, mode='w') as test_set:
+            test_set.add_input(self.tmp_tiff_stack, 'test_tif')
+
+        with h5py.File(self.tmp_training_set, 'r') as f:
+            assert 'input/test_tif' in f.keys(), \
+                'Error: Test image not found in training set!'
 
 
 if __name__ == '__main__':
