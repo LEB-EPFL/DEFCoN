6c00484aed329cffa39e3bafbfc38d47cd631758
Kyle M. Douglass <kyle.m.douglass@gmail.com>
2018-03-28 15:15:05 +0200

    Added basic infrastructure for CI and unit tests.

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -1,3 +1,15 @@
 # © All rights reserved. ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE,
-# Switzerland, Laboratory of Experimental Biophysics
+# Switzerland, Laboratory of Experimental Biophysics, 2018
 # See the LICENSE.txt file for more details.
+
+import unittest
+
+
+class TestTrainingSetMethods(unittest.TestCase):
+
+    def test_default(self):
+        assert True
+
+
+if __name__ == '__main__':
+    unittest.main()
