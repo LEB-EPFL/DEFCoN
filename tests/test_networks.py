7c5c32c01d84b931c7577ef5d019776ef3695888
Kyle M. Douglass <kyle.m.douglass@gmail.com>
2018-04-04 15:18:44 +0200

    Added more unit tests for the networks module.

--- tests/test_networks.py
+++ tests/test_networks.py
@@ -2,9 +2,12 @@
 # Switzerland, Laboratory of Experimental Biophysics, 2018
 # See the LICENSE.txt file for more details.
 
+import tempfile
 import unittest
 
+import numpy as np
 import pkg_resources
+import tifffile
 
 import defcon.networks as networks
 
@@ -33,10 +36,35 @@ class TestFCNMethodsIO(unittest.TestCase):
         """Obtains the full path to the DEFCoN model file.
 
         """
-        self.defcon_filename = pkg_resources.resource_filename('defcon.resources', 'defcon_tf13.h5')
+        # The DEFCoN model
+        defcon_filename = pkg_resources.resource_filename('defcon.resources', 'defcon_tf13.h5')
+        self.model = networks.FCN.from_file(defcon_filename)
 
-    def test_defcon_from_file(self):
-        """Can the DEFCoN FCN model be retrieved from a HDF file?
+        # Creates an image with a single Gaussian kernel in the center.
+        self.tmp_tiff_stack = tempfile.mkstemp(suffix='.tif')[1]
+        x = np.linspace(-10, 10, 32)
+        y = np.linspace(-10, 10, 32)
+        x, y = np.meshgrid(x, y)
+        sigma_x = 1
+        sigma_y = 1
+        self.ground_truth = 1 / 2 / sigma_x / sigma_y / np.pi * np.exp(
+            -x ** 2 / 2 / sigma_x ** 2 - y ** 2 / 2 / sigma_y ** 2)
+        self.ground_truth = np.uint16(self.ground_truth * 500).reshape(1, 32, 32) + 100
+        self.ground_truth += np.random.randint(0, 5, size=(32, 32), dtype='uint16')
+        tifffile.imsave(
+            self.tmp_tiff_stack,
+            self.ground_truth,
+            dtype='uint16')
+
+        tifffile.imsave(
+            '/home/douglass/Desktop/img.tif',
+            self.ground_truth,
+            dtype='uint16')
+
+    def test_predict_tiff(self):
+        """Verify that prediction from an image in a TIF file is approximately correct.
 
         """
-        model = networks.FCN.from_file(self.defcon_filename)
+        y_pred = self.model.predict_tiff(self.tmp_tiff_stack)
+        dens = np.sum(y_pred.squeeze())
+        np.testing.assert_approx_equal(1, dens, significant=1)
